3bc57c190bcb79de61687a4b5c7f306bdba0790a
zhangyi12384 <1528167459@qq.com>
2022-09-26 09:24:49 +0800

    feat:登录完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,28 @@
 // 对axios二次封装
 import axios from 'axios'
-
+import { Message } from 'element-ui'
 // 创建了一个新的axios实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // 超时时间
+})
+
+service.interceptors.response.use(response => {
+  // 1. 考虑把那些数据跑出去
+  // 2. 接口成功 并且 业务成功
+  // 3. 没有成功 Promise.reject 抛出错误
+  console.log(response)
+  const { data, message, success } = response.data
+  if (success) { // 业务逻辑成功
+    return data
+  }
+  // 业务没成功
+  Message.error(message)
+  return Promise.reject(new Error(message))
+}, error => {
+  Message.error(error.message)
+  return Promise.reject(error)
 })
 
 export default service // 导出axios实例
